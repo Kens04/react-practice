d94c942dda7679aef6076353413da8e86ed41267
John Doe <johndoe@example.com>
2022-11-12 21:03:38 +0900

    11-12

--- src/index.js
+++ src/index.js
@@ -3,8 +3,12 @@ import { App } from "./App";
 import { CssModules } from "./CssModules";
 import { StyledJsx } from "./StyledJsx";
 import { StyledComponents } from "./StyledComponents";
+import { Emotion } from "./Emotion";
+import { TailwindCss } from "./TailwindCss";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 ReactDOM.render(<CssModules />, document.getElementById("roots"));
 ReactDOM.render(<StyledJsx />, document.getElementById("test"));
 ReactDOM.render(<StyledComponents />, document.getElementById("test1"));
+ReactDOM.render(<Emotion />, document.getElementById("test2"));
+ReactDOM.render(<TailwindCss />, document.getElementById("test3"));
